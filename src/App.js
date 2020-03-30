bc5c2b484d41429193f61310f6b0700c54afb54a
Ajay <be.el.ajay@gmail.com>
2020-03-30 19:33:44 +0530

    initial msg

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What's up Mona & Madhuri !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ajayaradhya/friendscore"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FriendScore Github
         </a>
       </header>
     </div>
